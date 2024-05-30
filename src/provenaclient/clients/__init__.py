4e3b3ba0b630a45c61f0836d59cb70440e2aecc1
parth-kulkarni1 <parth.kulkarni2705@gmail.com>
2024-05-30 02:36:53 +0000

    Completed a base setup for the remaining API's (registry,prov) in respect to L2 and L3 and included all endpoints.

--- src/provenaclient/clients/__init__.py
+++ src/provenaclient/clients/__init__.py
@@ -1,3 +1,5 @@
 from provenaclient.clients.datastore_client import DatastoreClient
 from provenaclient.clients.search_client import SearchClient
 from provenaclient.clients.auth_client import AuthClient
+from provenaclient.clients.prov_client import ProvClient
+from provenaclient.clients.registry_client import RegistryClient
--- src/provenaclient/modules/__init__.py
+++ src/provenaclient/modules/__init__.py
@@ -2,3 +2,5 @@ from provenaclient.modules.datastore import Datastore
 from provenaclient.modules.search import Search
 from provenaclient.modules.auth import Auth
 from provenaclient.modules.link import Link
+from provenaclient.modules.prov import Prov
+from provenaclient.modules.registry import Registry
