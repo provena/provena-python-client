0871e2bd2a5c96f82343b2a3ab2d122db3e9785a
Peter Baker <peter.baker122@csiro.au>
2024-06-18 04:38:39 +0000

    Implementing the upload method and improving variable names

--- src/provenaclient/modules/submodules/datastore_io_submodule.py
+++ src/provenaclient/modules/submodules/datastore_io_submodule.py
@@ -53,20 +53,20 @@ class IOSubModule(ModuleService):
         # Clients related to the datastore scoped as private.
         self._datastore_client = datastore_client
 
-    async def download(
+    async def download_all_files(
         self,
-        destination_path: str,
+        destination_directory: str,
         dataset_id: str
     ) -> None:
         """
-        For a given dataset id.
-        
+        Downloads all files to the destination path for a given dataset id.
+
         - Fetches info
         - Fetches creds
         - Uses s3 cloud path lib to download all files to specified location
 
         Args:
-            destination_path (str): The destination path to save files to - use a directory
+            destination_directory (str): The destination path to save files to - use a directory
             dataset_id (str): The ID of the dataset to download files for - ensure you have read access
         """
         # Fetch the dataset information
@@ -88,6 +88,45 @@ class IOSubModule(ModuleService):
         # create path
         path = s3.S3Path(cloud_path=s3_location.s3_uri, client=client)
         # download to specified path
-        path.download_to(destination_path)
+        path.download_to(destination_directory)
+        # Release file handles
+        path.__del__
+
+    async def upload_all_files(
+        self,
+        source_directory: str,
+        dataset_id: str
+    ) -> None:
+        """
+        Uploads all files in the source path to the specified dataset id's storage location.
+
+        - Fetches info
+        - Fetches creds
+        - Uses s3 cloud path lib to upload all files to specified location
+
+        Args:
+            source_directory (str): The source path to upload files from - use a directory
+            dataset_id (str): The ID of the dataset to upload files for - ensure you have write access
+        """
+        # Fetch the dataset information
+        dataset_information = await self._datastore_client.fetch_dataset(
+            id=dataset_id
+        )
+        # Get the S3 location
+        assert dataset_information.item is not None, f"Expected non None item from dataset fetch, details: {dataset_information.status.details}."
+        s3_location = dataset_information.item.s3
+        # Get read credentials
+        write_creds = await self._datastore_client.generate_write_access_credentials(
+            write_access_credentials=CredentialsRequest(
+                dataset_id=dataset_id,
+                console_session_required=False
+            )
+        )
+        # setup s3 client
+        client = setup_s3_client(creds=write_creds)
+        # create path
+        path = s3.S3Path(cloud_path=s3_location.s3_uri, client=client)
+        # upload
+        path.upload_from(source_directory)
         # Release file handles
         path.__del__
